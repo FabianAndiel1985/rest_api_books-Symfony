705f6dc35947f10d37305e4be2be4d556cb031ab
Fabian Andiel <fabian.andiel@yahoo.com>
2022-11-26 17:07:26 +0100

    implemented custom error handling

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Throwable;
+
+class ErrorController
+{
+    public function show(Throwable $exception)
+     {
+         return new JsonResponse("smth went wrong");
+     }
+}
