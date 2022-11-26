1159de0273f4b2f84b290cacdbbed67b1f04d4c2
Fabian Andiel <fabian.andiel@yahoo.com>
2022-11-26 19:09:59 +0100

    added error handling for api

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -5,11 +5,15 @@ namespace App\Controller;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
 
 class ErrorController
 {
-    public function show(Throwable $exception)
+    public function show(HttpExceptionInterface $exception)
      {
-        return new JsonResponse("smth went wrong");
+        return new JsonResponse([
+            "httpErrorCode"=>$exception->getStatusCode(), 
+            "message"=>$exception->getMessage()]);
      }
 }
